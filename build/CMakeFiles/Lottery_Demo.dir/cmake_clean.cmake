80e734e1febb9b94cd561c46a41e45efc799fe3b
kolane <1784210334@qq.com>
2020-02-13 00:34:33 +0800

    进一步完善测试用例

---
+++ build/CMakeFiles/Lottery_Demo.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lottery_Demo.dir/Lottery.cpp.o"
+  "CMakeFiles/Lottery_Demo.dir/Staff.cpp.o"
+  "CMakeFiles/Lottery_Demo.dir/main.cpp.o"
+  "CMakeFiles/Lottery_Demo.dir/test.cpp.o"
+  "CMakeFiles/Lottery_Demo.dir/unittest.cpp.o"
+  "Lottery_Demo"
+  "Lottery_Demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lottery_Demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
